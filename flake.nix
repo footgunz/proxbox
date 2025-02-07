8f4672e7ee62defd5069d7bf7060fc2a948eb65d
dgunther <dgunther@users.noreply.github.com>
2025-02-07 15:15:28 -0500

    github

--- flake.nix
+++ flake.nix
@@ -11,6 +11,26 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = system: nixpkgs.legacyPackages.${system};
       version = builtins.readFile ./VERSION;
+
+      # Add this new attribute set for binary URLs and hashes
+      binaryUrls = {
+        "x86_64-linux" = {
+          url = "https://github.com/footgunz/proxbox/releases/download/v${version}/proxbox-linux-amd64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Placeholder, will be updated by GH Action
+        };
+        "aarch64-linux" = {
+          url = "https://github.com/footgunz/proxbox/releases/download/v${version}/proxbox-linux-arm64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+        "x86_64-darwin" = {
+          url = "https://github.com/footgunz/proxbox/releases/download/v${version}/proxbox-darwin-amd64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+        "aarch64-darwin" = {
+          url = "https://github.com/footgunz/proxbox/releases/download/v${version}/proxbox-darwin-arm64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+      };
     in
     {
       packages = forAllSystems (system:
@@ -37,6 +57,30 @@
               homepage = "https://github.com/footgunz/proxbox";
               license = licenses.mit;
               maintainers = [ ];
+            };
+          };
+
+          # Add the new binary package
+          binary = pkgs.stdenv.mkDerivation {
+            pname = "proxbox-bin";
+            inherit version;
+
+            src = pkgs.fetchurl (binaryUrls.${system});
+
+            dontUnpack = true;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src $out/bin/proxbox
+              chmod +x $out/bin/proxbox
+            '';
+
+            meta = with pkgs.lib; {
+              description = "HTTP and SOCKS proxy server (pre-built binary)";
+              homepage = "https://github.com/footgunz/proxbox";
+              license = licenses.mit;
+              maintainers = [ ];
+              platforms = [ system ];
             };
           };
         });
